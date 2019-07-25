c53808ac90d7604ef62319a332e8a2e1756cf101
Denis Kolesnikov <exsdis@gmail.com>
2019-07-26 01:52:34 +0300

    Fixed windows broken tests

--- test/helpers.js
+++ test/helpers.js
@@ -15,8 +15,8 @@ const ansiRegex = require('ansi-regex');
 module.exports = {
     STRING: TOKEN_COLORS[STRING](' ').match(ansiRegex()),
     NUMBER: TOKEN_COLORS[NUMBER](' ').match(ansiRegex()),
-    EMPTY_ARRAY: [...TOKEN_COLORS[LEFT_BRACKET](' ').match(ansiRegex()), ...TOKEN_COLORS[RIGHT_BRACKET](' ').match(ansiRegex())],
-    EMPTY_OBJECT: [...TOKEN_COLORS[LEFT_BRACE](' ').match(ansiRegex()), ...TOKEN_COLORS[RIGHT_BRACE](' ').match(ansiRegex())],
+    EMPTY_ARRAY: [...TOKEN_COLORS[LEFT_BRACKET](' ').match(ansiRegex()) || [], ...TOKEN_COLORS[RIGHT_BRACKET](' ').match(ansiRegex()) || []],
+    EMPTY_OBJECT: [...TOKEN_COLORS[LEFT_BRACE](' ').match(ansiRegex()) || [], ...TOKEN_COLORS[RIGHT_BRACE](' ').match(ansiRegex()) || []],
     NULL: TOKEN_COLORS[NULL](' ').match(ansiRegex()),
     FALSE: TOKEN_COLORS[FALSE](' ').match(ansiRegex()),
     TRUE: TOKEN_COLORS[TRUE](' ').match(ansiRegex())
